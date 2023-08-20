c2fc8acc69172006dda48b591dc1d7ee599cb14e
Abhishek <108579783+Abhishek6543jh@users.noreply.github.com>
2023-08-21 02:30:29 +0530

    mongoose v

--- index.js
+++ index.js
@@ -1,67 +1,45 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { MongoClient } from "mongodb";
+import mongoose from 'mongoose'; 
 const port =3000;
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
-// Replace the uri string with your connection string.
-const uri = "mongodb+srv://abhishek24:Abhishek20@abh.sr77f7j.mongodb.net/";
-const client = new MongoClient(uri);
-//k
+mongoose.connect("mongodb+srv://abhishek24:Abhishek20@abh.sr77f7j.mongodb.net/details?retryWrites=true&w=majority").then(()=>{
+  console.log("connected")}
+)
+//user schems
+const userSchema = new mongoose.Schema({
+  username:String,
+  email:String,
+  password:String
+})
+///en
 
-//function that performs after posting data with regester page
-async function uploadtodb(req,res) {
-    try {
-      const database = client.db('details');
-      await client.connect();
+///function to upload data to db when regesters
 
-      await createListing(client,{
-          username :req.body["username"],
-          email : req.body["email"],
-          password:req.body["password"]
-      });
-    
-          async function createListing(client, newListing){
-              const result = await client.db("details").collection("userdaaata").insertOne(newListing);
-              console.log(`New listing created with the following id: ${result.insertedId}`);
-          }
-  
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
+const user = mongoose.model("userdaaata",userSchema)
+const uploadtodb = async (req,res)=>{
+
+await user.create({ 
+username:req.body["username"],
+email:req.body["email"],
+password:req.body["password"] })
+}
+///en
+
+
+//function to chreck and authenticate login data
+const loginauth =async (res,username,password)=>{
+  const result = await user.exists({username:username,password:password})
+  if(result){
+    res.redirect("/")
   }
-
-//ended
-
-
-//function that validate user with login page details
-async function userdbauth(user,pass,req,res) {
-    try {
-      const database = client.db('details');
-      await client.connect();
-
-      await finduser(client,user,pass);
-
-    async function finduser(client,username,password){
-      const result=await client.db("details").collection("userdaaata").findOne({username:username,password:password});
-      if (result){
-        console.log("found");
-        res.redirect("/");
-      }
-      else{
-        console.log("not found");
-        res.redirect("/login");
-      }
-    }
-         
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
+  else{
+    res.redirect("/login")
   }
-//ended
-
+}
+//
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
@@ -74,11 +52,12 @@ app.get("/login",(req,res)=>{
 app.post("/login/auth",(req,res)=>{
    const user = req.body["username"];
    const pass= req.body["password"];
-   userdbauth(user,pass,req,res); 
+   loginauth(res,user,pass)
+  
 });
 app.post("/regester/reg",(req,res)=>{
-
-      uploadtodb(req,res).catch(console.dir);
+      uploadtodb(req,res)
+      
       res.redirect("/login");
 });
 app.listen(port,(req,res)=>{
